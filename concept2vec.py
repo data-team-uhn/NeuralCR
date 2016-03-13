9503e6a345f90b6ba761cb1435dcd91560b15ad0
Aryan Arbabi <arbabi@mail.cs.toronto.edu>
2016-03-12 23:43:00 -0500

    fixed not sharing the weights

--- concept2vec.py
+++ concept2vec.py
@@ -4,24 +4,17 @@ import numpy as np
 def conv2d(x, w):
 	return tf.nn.conv2d(x, w, [1, 1, 1, 1], "SAME")
 
-def weight_variable(shape):
-	return tf.Variable(tf.truncated_normal(shape, stddev = 0.1))
+def weight_variable(name, shape):
+	return tf.get_variable(name, shape = shape, initializer = tf.random_normal_initializer(stddev=0.1)) 
+	#	return tf.Variable(tf.truncated_normal(shape, stddev = 0.1))
 
-def bias_variable(shape):
-	return tf.Variable(tf.constant(0.1, shape=shape))
+def bias_variable(name, shape):
+	return tf.get_variable(name, shape = shape, initializer = tf.constant_initializer(0.1)) 
+#	return tf.Variable(tf.constant(0.1, shape=shape))
 
-def conv_layer(x, filter_shape):
-	conv_w = weight_variable(filter_shape)
-	conv_bias = bias_variable( [filter_shape[3]] )
-
-	conv = tf.nn.relu(conv2d(x, conv_w) + conv_bias)
-	pool = pool2d(conv)
-
-	return pool
-
-def conv_pool_layer(x, filter_shape):
-	conv_w = weight_variable(filter_shape)
-	conv_bias = bias_variable( [filter_shape[3]] )
+def conv_pool_layer(x, name, filter_shape):
+	conv_w = weight_variable(name+"weights", filter_shape)
+	conv_bias = bias_variable(name+"bias", [filter_shape[3]] )
 
 	conv = tf.nn.relu(conv2d(x, conv_w) + conv_bias)
 	pool = tf.nn.max_pool(conv, [1, 10, 1, 1], [1, 10, 1, 1], "SAME")
@@ -29,8 +22,9 @@ def conv_pool_layer(x, filter_shape):
 	return pool
 
 def fully_connected_layer(x, in_size, out_size):
-	dense_weights=weight_variable([in_size,out_size])
-	dense_bias=bias_variable([out_size])
+	dense_weights=weight_variable("dense_weights", [in_size,out_size])
+#	tf.histogram_summary("dense weights", dense_weights)
+	dense_bias=bias_variable("dense_bias",[out_size])
 
 	return tf.matmul(x,dense_weights) + dense_bias
 
@@ -38,10 +32,10 @@ def normalize(x):
 	return x / tf.sqrt( tf.reduce_sum( tf.square(x), 1, keep_dims=True ))
 
 def concept_vector_model(x, keep_prob):
-	layer_1gram = conv_pool_layer(x, [1, 1, 100, 35])
-	layer_2gram = conv_pool_layer(x, [2, 1, 100, 30])
-	layer_3gram = conv_pool_layer(x, [3, 1, 100, 20])
-	layer_4gram = conv_pool_layer(x, [4, 1, 100, 15])
+	layer_1gram = conv_pool_layer(x, "layer_1gram_", [1, 1, 100, 35])
+	layer_2gram = conv_pool_layer(x, "layer_2gram_", [2, 1, 100, 30])
+	layer_3gram = conv_pool_layer(x, "layer_3gram_", [3, 1, 100, 20])
+	layer_4gram = conv_pool_layer(x, "layer_4gram_", [4, 1, 100, 15])
 
 	full_layer = tf.concat(3, [layer_1gram, layer_2gram, layer_3gram, layer_4gram])
 	full_layer_dropout = tf.nn.dropout(full_layer,keep_prob)
--- conv.py
+++ conv.py
@@ -14,6 +14,7 @@ def main():
 	batch_size = 50
 
 	data = pickle.load(open('triplets','rb'))
+	#data = pickle.load(open('triplets_small','rb'))
 	reader = DataReader(data)
 
 	guide    = tf.placeholder(tf.float32, [None, word_size * num_of_words])
@@ -26,26 +27,33 @@ def main():
 
 	keep_prob = tf.placeholder(tf.float32)
 
-	g_v = concept_vector_model(g, keep_prob)
-	c0_v = concept_vector_model(c0, keep_prob)
-	c1_v = concept_vector_model(c1, keep_prob)
+	with tf.variable_scope("models"):
+		g_v = concept_vector_model(g, keep_prob)
+	with tf.variable_scope("models", reuse=True):
+		c0_v = concept_vector_model(c0, keep_prob)
+		c1_v = concept_vector_model(c1, keep_prob)
 	
-	y_ = tf.concat(1, [tf.reduce_sum (g_v * c0_v, 1, keep_dims=True), tf.reduce_sum (g_v * c1_v, 1, keep_dims=True)])
-	y_normalised = y_ / tf.reduce_sum(y_, 1, True)
+		y_ = tf.concat(1, [tf.reduce_sum (g_v * c0_v, 1, keep_dims=True), tf.reduce_sum (g_v * c1_v, 1, keep_dims=True)])
+		y_normalised = y_ / tf.reduce_sum(y_, 1, True)
 
-	y = tf.placeholder(tf.float32, [None, 2])
+		y = tf.placeholder(tf.float32, [None, 2])
 
-	cross_entropy = - tf.reduce_sum(y * tf.log(y_normalised))
+		cross_entropy = - tf.reduce_sum(y * tf.log(y_normalised))
 
-	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-	
-	correct_prediction = tf.equal(tf.arg_max(y, 1), tf.arg_max(y_normalised,1))
+		train_step = tf.train.AdamOptimizer(2e-5).minimize(cross_entropy)
+		
+		correct_prediction = tf.equal(tf.arg_max(y, 1), tf.arg_max(y_normalised,1))
 
-	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 	init_op=tf.initialize_all_variables()
 
+	merged = tf.merge_all_summaries()
+
+	writer_i=0
 	with tf.Session() as sess:
+		writer = tf.train.SummaryWriter("/u/arbabi/ncr_tmp",
+                                sess.graph.as_graph_def(add_shapes=True))
 		sess.run(init_op)
 		validation_set = reader.get_validation()
 		test_set = reader.get_test()
@@ -57,9 +65,13 @@ def main():
 				if new_batch == None:
 					break
 				if step%100 == 0:
-					print "Step:: ", step, "Accuracy:: ", sess.run(accuracy, feed_dict={guide:new_batch[0], concept0:new_batch[1], concept1:new_batch[2], y:new_batch[3], keep_prob:1.0})
+					res = sess.run(accuracy, feed_dict={guide:new_batch[0], concept0:new_batch[1], concept1:new_batch[2], y:new_batch[3], keep_prob:1.0})
+					#res = sess.run([merged, accuracy], feed_dict={guide:new_batch[0], concept0:new_batch[1], concept1:new_batch[2], y:new_batch[3], keep_prob:1.0})
+					print "Step:: ", step, "Accuracy:: ", res
+					#writer.add_summary(summary_str, writer_i)
 				sess.run(train_step, feed_dict={guide:new_batch[0], concept0:new_batch[1], concept1:new_batch[2], y:new_batch[3], keep_prob:0.7})
 				step+=1
+				writer_i+=1
 			
 			print "Epoch:: ", epoch, "Validation Accuracy:: ", sess.run(accuracy, feed_dict={guide:validation_set[0], concept0:validation_set[1], concept1:validation_set[2], y:validation_set[3], keep_prob:1.0})
 
