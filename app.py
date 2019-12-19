5aa5ea297c96e0ecb54553d72095aa746b7d9aae
Michael Lescisin <michael.lescisin@sickkids.ca>
2019-12-19 15:43:25 -0500

    Switch OBO parser to Orange
    Now works with MONDO ontology
    
    Lock pip package versions

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ NCR_MODELS['HPO'] = {}
 NCR_MODELS['HPO']['object'] = ncrmodel.NCR.loadfromfile('model_params', 'model_params/pmc_model_new.bin')
 NCR_MODELS['HPO']['threshold'] = 0.6
 
+NCR_MODELS['MONDO'] = {}
+NCR_MODELS['MONDO']['object'] = ncrmodel.NCR.loadfromfile('mondo_model_params', 'model_params/pmc_model_new.bin')
+NCR_MODELS['MONDO']['threshold'] = 0.6 #Just a copy+paste, should have better reasoning for selecting this value
+
 RUNNING_JOB_STATUS = {}
 
 running_job_id = 0
--- onto.py
+++ onto.py
@@ -1,80 +1,82 @@
 import numpy as np
 import re
 
-def _get_tag_value(line):
-  line = line[:line.find('!')]
-  col_index = line.find(':')
-  if col_index == -1:
-    return "", ""
-  tag = line[:col_index]
-  quotes=re.findall(r'\"(.+?)\"',line)
-  if len(quotes) == 0:
-    value = line[col_index+1:].strip()
-  else:
-    value = quotes[0].strip()
-  return tag, value
+from orangecontrib.bio.ontology import OBOOntology
 
-def _dfs(c, kids, mark):
-  mark.add(c)
-  for kid in kids[c]:
-    if kid not in mark:
-      _dfs(kid, kids, mark)
+def is_descendant_of(ont, itm, root):
+  try:
+    for t in ont.orange_model.super_terms(itm):
+      if t.id == root:
+        return True
+  except:
+    return False
+  return False
+
+def remove_from_list(lst, val):
+  lst_copy = []
+  for v in lst:
+    if v != val:
+      lst_copy.append(v)
+  return lst_copy
 
 class Ontology():
-
-  def _load_oboFile(self, oboFile, root):
-    names={}
-    def_text={}
-    kids={}
-    parents={}
-    real_id = {}
-    while True:
-      line=oboFile.readline()
-      if line == "":
-        break
-      tag, value = _get_tag_value(line)
-      if tag == "":
-        continue
-      if tag == "id":
-        concept_id = value
-        parents[concept_id] = []
-        kids[concept_id] = []
-        names[concept_id] = []
-        def_text[concept_id] = []
-        real_id[concept_id] = concept_id
-
-      if tag == "def":
-        def_text[concept_id].append(value)
-      if tag == "name" or tag == "synonym":
-        names[concept_id].append(value)
-      if tag == "alt_id":
-        real_id[value] = concept_id
+  def __init__(self, obo_address, root_id):
+    self.orange_model = OBOOntology(obo_address)
     
-    oboFile.seek(0)
-    while True:
-      line=oboFile.readline()
-      if line == "":
-        break
-      tag, value = _get_tag_value(line)
-      if tag == "":
+    self.concepts = []
+    self.names = {}
+    self.parents = {}
+    for t in self.orange_model.terms():
+      if t.id is None:
         continue
-      if tag=="id":
-        concept_id = value
+      if t.name is None:
+        continue
+      if (not is_descendant_of(self, t.id, root_id)) and (t.id != root_id):
+        continue
+      
+      self.concepts.append(t.id)
+      self.names[t.id] = [t.name]
+      for t_syn in t.synonyms:
+        self.names[t.id].append(t_syn)
+      self.parents[t.id] = []
+      for rel_edge, rel_obj in t.related_objects():
+        if rel_edge == 'is_a':
+          #Only append() parents that are descendants of root
+          if is_descendant_of(self, rel_obj, root_id) or rel_obj == root_id:
+            self.parents[t.id].append(rel_obj)
+    
+    #Do a clean-up, removing parents that are not in the self.concepts list
+    for c in self.concepts:
+      remove_these = []
+      for cp in self.parents[c]:
+        if cp not in self.concepts:
+          remove_these.append(cp)
+      
+      for itm in remove_these:
+        self.parents[c] = remove_from_list(self.parents[c], itm)
+    
+    self.concept2id = dict(zip(self.concepts,range(len(self.concepts))))
+    self.root_id = root_id
+    
+    self.name2conceptid = {}
+    for c in self.concepts:
+      for name in self.names[c]:
+        normalized_name = name.strip().lower()
+        self.name2conceptid[normalized_name] = self.concept2id[c]
 
-      if tag=="is_a":
-        parent_id = real_id[value]
-        kids[parent_id].append(concept_id)
-        parents[concept_id].append(parent_id)
-    mark=set()
-    _dfs(root, kids, mark)
+    self.ancestor_weight = {}
+    self.samples = []
+    for c in self.concepts:
+      self._update_ancestry_sparse(c)
 
-    self.names = {c:names[c] for c in mark}
-    self.def_text = {c:def_text[c] for c in mark if c in def_text}
-    self.parents = {c:parents[c] for c in mark}
-    self.kids = {c:kids[c] for c in mark}
-    self.real_id = real_id
-    for c in self.parents:
-      self.parents[c]=[p for p in parents[c] if p in mark]
+    self.sparse_ancestors = []
+    self.sparse_ancestors_values = []
+    for cid in self.ancestor_weight:
+      self.sparse_ancestors += [[cid, ancid]
+          for ancid in self.ancestor_weight[cid]]
+      self.sparse_ancestors_values += [self.ancestor_weight[cid][ancid]
+          for ancid in self.ancestor_weight[cid]]
+
 
   def _update_ancestry_sparse(self, c):
     cid = self.concept2id[c]
@@ -95,35 +97,4 @@ class Ontology():
                 self.ancestor_weight[pid][ancestor]/num_parents)
 
     return self.ancestor_weight[cid].keys()
-
-
-
-  def __init__(self, obo_address, root_id):
-    if isinstance(obo_address, str):
-      obo_file=open(obo_address)
-    else:
-      obo_file=obo_address
-    self._load_oboFile(obo_file, root_id)
-    self.concepts = [c for c in sorted(self.names.keys())]
-    self.concept2id = dict(zip(self.concepts,range(len(self.concepts))))
-    self.root_id = root_id
-
-    self.name2conceptid = {}
-    for c in self.concepts:
-      for name in self.names[c]:
-        normalized_name = name.strip().lower()
-        self.name2conceptid[normalized_name] = self.concept2id[c]
-
-    self.ancestor_weight = {}
-    self.samples = []
-    for c in self.concepts:
-      self._update_ancestry_sparse(c)
-
-    self.sparse_ancestors = []
-    self.sparse_ancestors_values = []
-    for cid in self.ancestor_weight:
-      self.sparse_ancestors += [[cid, ancid]
-          for ancid in self.ancestor_weight[cid]]
-      self.sparse_ancestors_values += [self.ancestor_weight[cid][ancid]
-          for ancid in self.ancestor_weight[cid]]
 
