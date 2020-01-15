31ec494da88c244aa9e3c36738306aa1e1992002
Michael Lescisin <michael.lescisin@sickkids.ca>
2020-01-15 16:10:58 -0500

    Submitted NCR training jobs are saved to an envvar specified WebDAV server
    These training jobs (running on HPF) write their trained models to the WebDAV server
    
    The NCR Web Application checks the WebDAV server for completed model training jobs, then saves and imports them
    
    Due to memory constraints, NCR models are only loaded from disk when needed. The models are deleted from memory once they are no longer needed

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,24 @@
 FROM python:3.6-slim-buster
 
 # Install gcc, needed to build fasttext
-RUN apt-get update && apt-get install -y g++ wget wait-for-it
+RUN apt-get update && apt-get install -y \
+	g++ \
+	wget \
+	wait-for-it \
+	libcurl4-openssl-dev \
+	libssl-dev
 
 # Install dependencies
-RUN pip3 install 'cython==0.29.14' 'scipy==1.4.0' 'tensorflow==1.13.2' 'fasttext==0.9.1' 'Flask==1.1.1' 'Orange-Bioinformatics==2.6.25' 'nested-lookup==0.2.19'
+RUN pip3 install 'cython==0.29.14' 'scipy==1.4.0' 'tensorflow==1.13.2' 'fasttext==0.9.1' 'Flask==1.1.1' 'Orange-Bioinformatics==2.6.25' 'nested-lookup==0.2.19' 'pycurl==7.43.0.3'
 
 # Put everything in /opt/ncr
 RUN mkdir -p /root/opt/ncr/
 WORKDIR /root/opt/ncr/
 COPY . ./
+RUN mkdir new_model_params/
+RUN mkdir /root/uploaded_obo
+RUN mkdir /root/trained_model_param
+RUN mkdir /root/qsub
 
 # This is the default command executed when starting the container
 COPY startup_script.sh /
---
+++ json_train.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Trains an NCR model based on training data associated with the
+given JOB ID.
+"""
+
+import os
+import sys
+import json
+
+import train
+
+CONST_HOMEDIR = os.environ['HOME']
+
+CONST_QSUB_FILEPATH = "{}/qsub".format(os.environ['HOME'])
+JOB_ID = int(sys.argv[1])
+
+#Set CWD
+os.chdir(CONST_QSUB_FILEPATH)
+
+#Load the JSON file associated with this
+f_json = open("{}.json".format(JOB_ID), 'r')
+training_args_dict = json.loads(f_json.read())
+f_json.close()
+
+#Resolve the $HOME directory
+for key in training_args_dict.keys():
+  if type(training_args_dict[key]) == str:
+    training_args_dict[key] = training_args_dict[key].format(CONST_HOMEDIR)
+
+#Log info
+print("Running NCR training with params:")
+for key in training_args_dict.keys():
+  print("\t{} --> {}".format(key, training_args_dict[key]))
+
+#Start the training
+training_args = train.MainTrainArgClass(**training_args_dict)
+train.main_train(training_args, JOB_ID)
